923dc09059a3d63bd51131a0e7b0cc548607828e
Scott Mansfield <sgmansf@gmail.com>
2016-03-28 21:15:38 -0700

    Adding initial code, tests, and deps

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/shopspring/decimal"
+)
+
+const port = 7001
+
+var one = decimal.NewFromFloat(1)
+
+func main() {
+	http.HandleFunc("/", add1)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func add1(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		rw.Header().Add("Allow", "GET")
+		http.Error(rw, "Method Not Allowed", 405)
+		return
+	}
+
+	in := req.URL.Query().Get("num")
+
+	if strings.TrimSpace(in) == "" {
+		http.Error(rw, "Bad Request", 400)
+		return
+	}
+
+	if num, ok := decimal.NewFromString(in); ok {
+		num = num.Add(num, one)
+		fmt.Fprint(rw, num.String())
+		return
+	}
+
+	// all else fails, it was a bad request
+	http.Error(rw, "Bad Request", 400)
+}
---
+++ main_integ_test.go
@@ -0,0 +1,112 @@
+// needs to be the same package to access main()
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	go main()
+	<-time.After(100 * time.Millisecond)
+	os.Exit(m.Run())
+}
+
+func TestSmallInt(t *testing.T) {
+	res, err := http.Get("http://localhost:7001/?num=1234")
+	if err != nil {
+		t.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Response was not a 200. Error: %s", res.Status)
+	}
+
+	bodybytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Error reading response: %s", err.Error())
+	}
+
+	body := string(bodybytes)
+
+	if body != "1235" {
+		t.Fatalf("Got unexpected result: %s", body)
+	}
+
+	t.Log("Success")
+}
+
+func TestBigInt(t *testing.T) {
+	res, err := http.Get("http://localhost:7001/?num=123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")
+	if err != nil {
+		t.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Response was not a 200. Error: %s", res.Status)
+	}
+
+	bodybytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Error reading response: %s", err.Error())
+	}
+
+	body := string(bodybytes)
+
+	if body != "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567891" {
+		t.Fatalf("Got unexpected result: %s", body)
+	}
+
+	t.Log("Success")
+}
+
+func TestSmallFloat(t *testing.T) {
+	res, err := http.Get("http://localhost:7001/?num=0.1234")
+	if err != nil {
+		t.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Response was not a 200. Error: %s", res.Status)
+	}
+
+	bodybytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Error reading response: %s", err.Error())
+	}
+
+	body := string(bodybytes)
+
+	if body != "1.1234" {
+		t.Fatalf("Got unexpected result: %s", body)
+	}
+
+	t.Log("Success")
+}
+
+func TestBigFloat(t *testing.T) {
+	res, err := http.Get("http://localhost:7001/?num=1.234e100")
+	if err != nil {
+		t.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Response was not a 200. Error: %s", res.Status)
+	}
+
+	bodybytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Error reading response: %s", err.Error())
+	}
+
+	body := string(bodybytes)
+
+	if body != "1.1234" {
+		t.Fatalf("Got unexpected result: %s", body)
+	}
+
+	t.Log("Success")
+}
