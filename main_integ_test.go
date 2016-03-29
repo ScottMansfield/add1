5cfe99669c5315bbcdd1f30fefb662930e250458
Scott Mansfield <sgmansf@gmail.com>
2016-03-28 22:08:13 -0700

    More testing

--- main_integ_test.go
+++ main_integ_test.go
@@ -104,8 +104,35 @@ func TestBigFloat(t *testing.T) {
 
 	body := string(bodybytes)
 
-	if body != "1.1234" {
+	if body != "12340000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001" {
 		t.Fatalf("Got unexpected result: %s", body)
+	}
+
+	t.Log("Success")
+}
+
+func TestBadNumberFormat(t *testing.T) {
+	res, err := http.Get("http://localhost:7001/?num=abcd")
+	if err != nil {
+		t.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	if res.StatusCode != 400 {
+		t.Fatalf("Response was not a 200. Error: %s", res.Status)
+	}
+
+	t.Log("Success")
+}
+
+func TestHexNumber(t *testing.T) {
+	res, err := http.Get("http://localhost:7001/?num=0xff")
+	if err != nil {
+		t.Fatalf("Error calling server: %s", err.Error())
+	}
+
+	// Ideally, I could parse hexadecimal, octal, and all other bases but oh well
+	if res.StatusCode != 400 {
+		t.Fatalf("Response was not a 200. Error: %s", res.Status)
 	}
 
 	t.Log("Success")
